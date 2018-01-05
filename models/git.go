d4265cfc049542ee1fbd3b05cc3417d9dd7b579d
Emad Elsaid <blazeeboy@gmail.com>
2018-01-05 14:54:19 +0100

    shorten variable name

--- models/git.go
+++ models/git.go
@@ -23,19 +23,19 @@ func Contributors() [][]interface{} {
 	logs, err := repo.Log(&git.LogOptions{})
 	checkError(err)
 
-	commitsCountPerAuthor := newCounter()
+	commits := newCounter()
 	err = logs.ForEach(func(c *object.Commit) error {
-		commitsCountPerAuthor.add(c.Author.Name)
+		commits.add(c.Author.Name)
 		return nil
 	})
 
 	var s sortable
-	for k, v := range commitsCountPerAuthor.count {
+	for k, v := range commits.count {
 		s = append(s, sortableRow{key: v, value: k})
 	}
 	s.Sort()
 
-	output := make([][]interface{}, 0, len(commitsCountPerAuthor.count))
+	output := make([][]interface{}, 0, len(commits.count))
 	for _, row := range s {
 		output = append(output, []interface{}{row.value.(string), row.key})
 	}
