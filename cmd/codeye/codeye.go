a966ba7521568a9180a999fc5f0ff63ca79a1f75
Emad Elsaid <blazeeboy@gmail.com>
2018-01-05 11:31:57 +0100

    basic http server

--- cmd/codeye/codeye.go
+++ cmd/codeye/codeye.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
-	fmt.Printf("Hello World")
+	http.HandleFunc("/", handler)
+
+	log.Println("Serving on port :3000")
+	http.ListenAndServe(":3000", nil)
 }
