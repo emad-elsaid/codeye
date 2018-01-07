05124c3c1f466d46fb7b5a9c1713189779fa82a9
Emad Elsaid <blazeeboy@gmail.com>
2018-01-07 03:36:41 +0100

    open any free port and open browser for the user automatically

--- cmd/codeye/codeye.go
+++ cmd/codeye/codeye.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 
 	"github.com/emad-elsaid/codeye/controllers"
+	"github.com/emad-elsaid/codeye/utils"
 )
 
 func main() {
 	http.HandleFunc("/", controllers.Home)
 
-	log.Println("Serving on port :3000")
-	http.ListenAndServe(":3000", nil)
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Using port:")
+
+	url := fmt.Sprintf("http://localhost:%d", listener.Addr().(*net.TCPAddr).Port)
+	log.Printf("Serving on: %s\n", url)
+	go utils.OpenBrowser(url)
+	http.Serve(listener, nil)
 }
